da12202205c847e44c94d4db0deab7c5ae21d1d5
Carlos <carlos_alberto_lira@hotmail.com>
2021-05-13 20:31:49 -0500

    first commit

---
+++ src/components/CarComposition.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>Coche - Composition</h2>
+  <p>Marca: {{ brand }}</p>
+  <p>Modelo: {{ model }}</p>
+  <p>Potencia: {{ power }}</p>
+  <button @click="upPower">Aumentar</button>
+  <button @click="downPower(300)">Disminuir</button>
+</template>
+
+<script>
+export default {
+  setup() {
+    const brand = "Audi";
+    const model = "A4";
+    const power = 40;
+
+    const upPower = () => {
+      console.log("Aumentar potencia...");
+    };
+
+    const downPower = (newPower) => {
+      console.log("Bajar potencia..." + newPower);
+    };
+
+    return {
+      brand,
+      model,
+      power,
+      upPower,
+      downPower,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/CarOptions.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Coche - Options API</h2>
+  <p>Marca: {{ brand }}</p>
+  <p>Modelo: {{ model }}</p>
+  <p>Potencia: {{ power }}</p>
+
+  <button @click="upPower(100)">Aumentar</button>
+  <button @click="downPower">Disminuir</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      brand: "Seat",
+      model: "Leon",
+      power: 50,
+    };
+  },
+  methods: {
+    upPower(newPower) {
+      console.log("Aumentando potencia ->" + newPower);
+    },
+    downPower() {
+      console.log("Disminuir potencia");
+    },
+  },
+};
+</script>
+
+<style></style>
